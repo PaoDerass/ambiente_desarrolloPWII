dbb54b2f0d4c6a7d0100b433f5511e7b21b16f3d
PaoDerass <paola_deras@unitec.edu>
2025-02-05 22:06:40 -0600

    Añade el cálculo de la fecha estimada de entrega usando moment

--- CalcularEnvio.js
+++ CalcularEnvio.js
@@ -1,20 +1,27 @@
 
-function calcularCostoEnvio(peso, distancia) {
-    const costoBase = 50.00;
-    let recargoPeso = 0;
-    let recargoDistancia = 0;
-  
- 
-    if (peso > 5) {
-      recargoPeso = (peso - 5) * 20.00;
-    }
-  
-   
-    if (distancia > 50) {
-      recargoDistancia = (distancia - 50) * 10.00;
-    }
-  
+const moment = require('moment');
 
-    const costoTotal = costoBase + recargoPeso + recargoDistancia;
+
+function calcularCostoEnvio(peso, distancia) {
+  const costoBase = 50.00;
+  let recargoPeso = 0;
+  let recargoDistancia = 0;
+
+
+  if (peso > 5) {
+    recargoPeso = (peso - 5) * 20.00;
   }
-  
+
+
+  if (distancia > 50) {
+    recargoDistancia = (distancia - 50) * 10.00;
+  }
+
+
+  const costoTotal = costoBase + recargoPeso + recargoDistancia;
+
+
+  const fechaHoy = moment();
+  const tiempoTransporte = Math.ceil(distancia / 100); 
+  const fechaEntrega = fechaHoy.add(2 + tiempoTransporte, 'days').format('YYYY-MM-DD');
+}
