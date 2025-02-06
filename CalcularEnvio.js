4afe6ef215e8c95f5fc7810088eac2de473654db
PaoDerass <paola_deras@unitec.edu>
2025-02-05 21:59:13 -0600

    Implementa la lógica de cálculo del costo de envío según peso y distancia

--- CalcularEnvio.js
+++ CalcularEnvio.js
@@ -3,5 +3,18 @@ function calcularCostoEnvio(peso, distancia) {
     const costoBase = 50.00;
     let recargoPeso = 0;
     let recargoDistancia = 0;
+  
+ 
+    if (peso > 5) {
+      recargoPeso = (peso - 5) * 20.00;
+    }
+  
+   
+    if (distancia > 50) {
+      recargoDistancia = (distancia - 50) * 10.00;
+    }
+  
+
+    const costoTotal = costoBase + recargoPeso + recargoDistancia;
   }
   
